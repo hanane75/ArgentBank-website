16f555c1c2ea909880d88787310e68a1e9145945
SALHI Hanane <benkada.hanane@gmail.com>
2024-09-10 18:35:04 +0200

    creat protectroute

--- FRONT-END/front-end/src/App.js
+++ FRONT-END/front-end/src/App.js
@@ -3,6 +3,7 @@ import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-d
 import HomePage from './page/HomePage'; // Page d'accueil
 import SignUp from './page/SignUp'; // Page de connexion
 import UserPage  from './page/UserPage';
+import ProtectedRoute from './components/ProtectedRoute';
 import './App.css';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
     <Route path="/" element={<Navigate replace to="/HomePage" />} />
     <Route path="/HomePage" element={<HomePage />} />
     <Route path="/SignUp" element={<SignUp />} /> 
-    <Route path="/UserPage" element={<UserPage />} /> 
+    <Route path="/UserPage" element={<ProtectedRoute> <UserPage /></ProtectedRoute> } />
   </Routes>
 </Router>
   );
